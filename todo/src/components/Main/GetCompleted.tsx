c2af25c0b14f6605920e4c722e96fb70947e41d0
NistonN <nisprogrammer@gmail.com>
2024-08-20 22:59:28 +0300

    Main 2

--- todo/src/components/Main/GetCompleted.tsx
+++ todo/src/components/Main/GetCompleted.tsx
@@ -4,29 +4,25 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../state/store";
 
 export const GetCompleted = () => {
+  const db = useSelector((state: RootState) => state.db);
 
-    const db = useSelector((state: RootState) => state.db);
+  const [data, setData]: any = useState(db);
 
-    const [data, setData]: any = useState(db)
+  useEffect(() => {
+    setData(db);
+  }, [db]);
 
-    useEffect(() => {
-        setData(db)
-    },[db])
-
-    return <>
+  return (
+    <>
+      <div className={ style.complete }>
         <h1>Completed:</h1>
-        {
-            data.map((note: any) => (
-                <div key={note.id}>
-                    {note.status === true && ( 
-                        <>
-                            <div>Id = {note.id}</div>
-                            <div>{note.name}</div>
-                        </>
-                    )}
-
-                </div>
-            ))
-        }
+        {data.map((note: any) => (
+          <div key={note.id}>
+            <div>Id = {note.id}</div>
+            <div>{note.name}</div>
+          </div>
+        ))}
+      </div>
     </>
+  );
 };
