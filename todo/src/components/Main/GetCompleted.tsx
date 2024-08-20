b6462f0e9548607fa0db08fa20e706f3c889b2ce
Tim <Obertim@yandex.ru>
2024-08-20 23:44:53 +0300

    fix comp component

--- todo/src/components/Main/GetCompleted.tsx
+++ todo/src/components/Main/GetCompleted.tsx
@@ -2,27 +2,33 @@ import style from "./Main.module.scss";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../state/store";
+import { DeleteNote } from "./notes/DeleteNote";
 
 export const GetCompleted = () => {
-  const db = useSelector((state: RootState) => state.db);
+    const db = useSelector((state: RootState) => state.db);
 
-  const [data, setData]: any = useState(db);
+    const [data, setData]: any = useState(db);
 
-  useEffect(() => {
-    setData(db);
-  }, [db]);
+    useEffect(() => {
+        setData(db);
+    }, [db]);
 
-  return (
-    <>
-      <div className={ style.complete }>
-        <h1>Completed:</h1>
-        {data.map((note: any) => (
-          <div key={note.id}>
-            <div>Id = {note.id}</div>
-            <div>{note.name}</div>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={style.complete}>
+                <h1>Completed:</h1>
+                {data.map((note: any) => (
+                    <div key={note.id}>
+                        {note.status === true && (
+                            <>
+                                <div>{note.name}</div>
+                                <div>{new Date(note.time).toLocaleString()}</div>
+                                <DeleteNote id={note.id} />
+                            </>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 };
