b9c084bba950d20b9ee1d696d27ec3937e3e5082
Tim <Obertim@yandex.ru>
2024-08-20 14:18:01 +0300

    add redux

--- todo/src/components/Main/GetAnyNote.tsx
+++ todo/src/components/Main/GetAnyNote.tsx
@@ -4,18 +4,20 @@ import { DeleteNote } from "./DeleteNote"
 import { EditNote } from "./EditNote"
 
 export const GetAnyNote = () => {
-    const [data, setData]: any = useState([])
+    const [data, setData]: any = useState(db)
 
     useEffect(() => {
-        setData(db)    
+        const updatedData = [...db]
+        setData(updatedData);
     }, [])
 
     return <>
         {
             data.map((note: any) => (
                 <div key={note.id}>
-                    <div>{note.id}</div>
+                    <div>Id = {note.id}</div>
                     <div>{note.name}</div>
+                    <div>{new Date(note.time).toLocaleString()}</div>
                     <EditNote id={note.id}/>
                     <DeleteNote id={note.id} />
                 </div>
